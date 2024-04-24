9e4a8bb7c837f70cfea409087b106270abd8d9d2
권정훈 <righthun.kwon@gmail.com>
2024-04-24 21:48:06 +0900

    boj solve

--- baekjoon/01-basic/1001.js
+++ baekjoon/01-basic/1001.js
@@ -1,3 +1,3 @@
 let fs = require('fs');
 let input = fs.readFileSync('../testcase.txt').toString().split(' ');
-console.log(parseInt(input[0]) + parseInt(input[1]));
+console.log(parseInt(input[0]) - parseInt(input[1]));
