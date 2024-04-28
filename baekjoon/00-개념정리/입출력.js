038f9899426ae94f448b6b8e6fa74f2a94627925
권정훈 <righthun.kwon@gmail.com>
2024-04-28 23:52:37 +0900

    use const

--- baekjoon/00-개념정리/입출력.js
+++ baekjoon/00-개념정리/입출력.js
@@ -3,9 +3,9 @@
 // baekjoon : /dev/stdin
 
 // 한 줄 입력
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
 
 // 여러 줄 입력
-let fs = require('fs');
-let inputs = fs.readFileSync('/dev/stdin').toString().split('\n');
+const fs = require('fs');
+const inputs = fs.readFileSync('/dev/stdin').toString().split('\n');
