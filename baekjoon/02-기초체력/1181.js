7aad3ef80927c35167a84dccaaaa0cd483515849
권정훈 <righthun.kwon@gmail.com>
2024-05-02 23:11:24 +0900

    boj solve

---
+++ baekjoon/02-기초체력/1181.js
@@ -0,0 +1,18 @@
+// 단어 정렬
+const fs = require('fs');
+const [N, words] = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+words = Array.from(new Set(words)).sort((a, b) => {
+  if (a.length > b.length) {
+    return 1;
+  } else if (a.length < b.length) {
+    return -1;
+  } else {
+    if (a > b) {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+});
+console.log(words.join('\n'));
