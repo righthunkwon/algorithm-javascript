dd7636427363f1a0973497a74d04123f5fe99d17
권정훈 <righthun.kwon@gmail.com>
2024-05-23 23:27:23 +0900

    boj solve

---
+++ baekjoon/01-기초체력/9498.js
@@ -0,0 +1,14 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+const score = +input;
+
+if (score >= 90) {
+  console.log('A');
+} else if (score >= 80) {
+  console.log('B');
+} else if (score >= 70) {
+  console.log('C');
+} else if (score >= 60) {
+  console.log('D');
+} else {
+  console.log('F');
+}
