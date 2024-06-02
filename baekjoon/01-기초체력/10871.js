b018e6a4372ef75b6937ab9b98d611d105d3b7e3
권정훈 <righthun.kwon@gmail.com>
2024-06-03 00:36:41 +0900

    boj solve

--- baekjoon/01-기초체력/10871.js
+++ baekjoon/01-기초체력/10871.js
@@ -2,11 +2,5 @@ const input = require('fs').readFileSync(0).toString().trim().split('\n');
 const [N, X] = input[0].split(' ').map((item) => +item);
 const arr = input[1].split(' ').map((item) => +item);
 
-let result = '';
-for (i = 0; i < N; i++) {
-  if (arr[i] < X) {
-    result += arr[i] + ' ';
-  }
-}
-
-console.log(result);
+const result = arr.filter((item) => item < X);
+console.log(result.join(' '));
