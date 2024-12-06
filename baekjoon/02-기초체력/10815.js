3bd1abab4fdfed0b44783c145e5dce6d5ff432e1
권정훈 <righthun.kwon@gmail.com>
2024-12-06 22:54:24 +0900

    boj solve

--- baekjoon/02-기초체력/10815.js
+++ baekjoon/02-기초체력/10815.js
@@ -1,12 +1,39 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'tc.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+const N = +input[0];
+const cards = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+const M = +input[2];
+const targets = input[3].split(' ').map(Number);
 
-const cards = new Set(input[1].split(' '));
-const targets = input[3].split(' ');
-const result = [];
+function binarySearch(target) {
+  let st = 0;
+  let ed = N - 1;
 
-for (const target of targets) {
-  cards.has(target) ? result.push(1) : result.push(0);
+  while (st <= ed) {
+    const md = Math.floor((st + ed) / 2);
+    if (target === cards[md]) {
+      return 1;
+    } else if (target > cards[md]) {
+      st = md + 1;
+    } else {
+      ed = md - 1;
+    }
+  }
+  return 0;
 }
-
+let result = [];
+for (const target of targets) {
+  result.push(binarySearch(target));
+}
 console.log(result.join(' '));
+
+// const cards = new Set(input[1].split(' '));
+// const targets = input[3].split(' ');
+// const result = [];
+// for (const target of targets) {
+//   cards.has(target) ? result.push(1) : result.push(0);
+// }
+// console.log(result.join(' '));
