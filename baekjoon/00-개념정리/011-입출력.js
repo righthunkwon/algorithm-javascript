64861c8fb028a7bbb2c1520e6ebbab895f2d0b83
권정훈 <righthun.kwon@gmail.com>
2024-12-10 20:56:34 +0900

    file path를 활용한 입출력

--- baekjoon/00-개념정리/011-입출력.js
+++ baekjoon/00-개념정리/011-입출력.js
@@ -2,10 +2,14 @@
 // vscode   : ../testcase.txt
 // baekjoon : /dev/stdin
 
-// 한 줄 입력
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+// 입력
+const filePath = process.platform === 'linux' ? '/dev/stdin' : 'tc.txt';
+const input = require('fs').readFileSync(filePath).toString().trim().split(' ');
 
-// 여러 줄 입력
-const fs = require('fs');
-const inputs = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+// 한 줄 입력
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+
+// // 여러 줄 입력
+// const fs = require('fs');
+// const inputs = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
