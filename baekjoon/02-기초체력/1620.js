cc385399317fbe4488c1d58128f7b2da528876f6
권정훈 <righthun.kwon@gmail.com>
2024-12-14 11:35:21 +0900

    boj solve

--- baekjoon/02-기초체력/1620.js
+++ baekjoon/02-기초체력/1620.js
@@ -1,18 +1,22 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'tc.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 const [N, M] = input[0].split(' ').map(Number);
-const dictionary = input.slice(1, N + 1).map((v) => v.trim());
-const question = input.slice(1 + N);
+const dict = input.slice(1, N + 1).map((v) => v.trim());
+const ques = input.slice(N + 1).map((v) => v.trim());
 
-const map = new Map(dictionary.map((v, i) => [v, i + 1]));
+dict.map((v, i) => {
+  return [v, i + 1];
+});
+
+const map = new Map(dict.map((v, i) => [v, i + 1]));
 const answer = [];
 
-question.forEach((v) => {
+ques.forEach((v) => {
   if (Number.isNaN(+v)) {
     answer.push(map.get(v));
   } else {
-    answer.push(arr[+v - 1]);
+    answer.push(dict[+v - 1]);
   }
 });
-console.log(answer);
+
 console.log(answer.join('\n'));
