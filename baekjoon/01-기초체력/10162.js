14cc128d0c00516fc8d586b4a793903949a9d8e7
권정훈 <righthun.kwon@gmail.com>
2024-12-23 23:28:46 +0900

    백준_전자레인지_그리디

--- baekjoon/01-기초체력/10162.js
+++ baekjoon/01-기초체력/10162.js
@@ -1,16 +1,9 @@
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'tc.txt';
 let input = +require('fs').readFileSync(filePath).toString().trim();
-
+let arr = [300, 60, 10];
 let answer = '';
-answer += Math.floor(input / 300) + ' ';
-input %= 300;
-answer += Math.floor(input / 60) + ' ';
-input %= 60;
-answer += Math.floor(input / 10);
-input %= 10;
-
-if (input !== 0) {
-  answer = -1;
-}
-
+arr.forEach((elm) => {
+  answer += Math.floor(input / elm) + ' ';
+  input %= elm;
+});
 console.log(answer);
