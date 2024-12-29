639a705c5a4ddf65a0b637d8c917e0e4815d8224
권정훈 <righthun.kwon@gmail.com>
2024-12-29 14:37:02 +0900

    분할정복복

--- baekjoon/00-개념정리/061-분할정복.js
+++ baekjoon/00-개념정리/061-분할정복.js
@@ -1,47 +1,42 @@
 /**
  * @분할정복
- * 동일한 로직을 작은 부분에서 재귀적으로 실행(참고: 백준 2630번)
+ * 문제를 작은 단위로 나누고 각각의 하위 문제를 해결한 후 이를 결합하여 최종적으로 원하는 문제를 해결
+ *
+ * 1.분할
+ * 2.정복
+ * 3.결합
  *
  */
 
-const filePath = process.platform === 'linux' ? '/dev/stdin' : 'tc.txt';
-const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
-
-const N = +input[0];
-const paper = input.slice(1).map((v) => v.split(' ').map(Number));
-let white = 0;
-let blue = 0;
-
-// 재귀호출 함수
-const recursion = (x, y, size) => {
-  // 색이 같을 경우 색종이 개수 증가
-  if (checkColor(x, y, size)) {
-    if (paper[x][y] === 0) white++;
-    else if (paper[x][y] === 1) blue++;
-    return;
+/**
+ * @일반함수
+ * O(n)의 시간복잡도, 30번 연산 수행
+ *
+ */
+function power1(base, exponent) {
+  let result = 1;
+  for (let i = 0; i < exponent; i++) {
+    result *= base;
   }
+  return result;
+}
+console.log(power1(2, 30)); // 1073741824
 
-  // 색이 같지 않을 경우 재탐색(분할정복)
-  const newSize = size / 2;
-  recursion(x, y, newSize); // 제1사분면
-  recursion(x, y + newSize, newSize); // 제2사분면
-  recursion(x + newSize, y, newSize); // 제3사분면
-  recursion(x + newSize, y + newSize, newSize); // 제4사분면
-};
-
-// 색이 같은지 여부를 검사하는 함수
-const checkColor = (x, y, size) => {
-  const standard = paper[x][y]; // 기준색
-
-  for (let i = x; i < x + size; i++) {
-    for (let j = y; j < y + size; j++) {
-      if (paper[i][j] !== standard) {
-        return false;
-      }
-    }
+/**
+ * @분할정복
+ * O(log n)의 시간복잡도
+ * n이 짝수일 떄와 홀수일 때를 분할하여 계산
+ *
+ */
+function power2(base, exponent) {
+  if (exponent === 0) {
+    return 1;
   }
-  return true;
-};
-
-recursion(0, 0, N);
-console.log(`${white}\n${blue}`);
+  if (exponent % 2 === 0) {
+    const half = power2(base, Math.floor(exponent / 2));
+    return half * half;
+  } else {
+    return base * power2(base, exponent - 1);
+  }
+}
+console.log(power2(2, 30)); // 1073741824
